ab8bc0831dd26a33632f4696465ba72e870c74dd
mouaztaloo00 <moaztalo14@gmail.com>
2024-09-11 15:16:18 +0300

    scroll bar by mouaz

--- src/components/minBar/ShowMiniNavbar.js
+++ src/components/minBar/ShowMiniNavbar.js
@@ -1,36 +1,91 @@
 import React from 'react';
-import { AppBar, Toolbar, Button, Container } from '@mui/material';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+
 const ShowMiniNavbar = () => {
   const { t } = useTranslation();
 
   return (
     <AppBar position="static" color="primary" sx={{ maxWidth: '1200px', margin: 'auto', borderRadius: 2 }}>
-      <Toolbar sx={{ justifyContent: 'space-around' }}>
-        <Container sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Button component={Link} to="/show/show_doctors" color="inherit" sx={{ mx: 2 }}>
+      <Toolbar
+        sx={{
+          display: 'flex',
+          overflowX: 'auto',
+          padding: 1,
+          alignItems: 'center', // Center items vertically
+          scrollbarWidth: 'thin', // For Firefox
+          '&::-webkit-scrollbar': {
+            height: 8, // For WebKit browsers
+          },
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: '#888',
+            borderRadius: 4,
+          },
+          '&::-webkit-scrollbar-track': {
+            backgroundColor: '#e0e0e0',
+            borderRadius: 4,
+          },
+        }}
+      >
+        <Box sx={{ display: 'flex', gap: 9, justifyContent: 'center', flexGrow: 1 }}>
+          <Button 
+            component={Link} 
+            to="/show/show_doctors" 
+            color="inherit" 
+            sx={{ fontSize: '1rem', textTransform: 'capitalize', py: 1, px: 2 }}
+          >
             {t('navbar.Doctors')}
           </Button>
-          <Button component={Link} to="/show/show_labs" color="inherit" sx={{ mx: 2 }}>
+          <Button 
+            component={Link} 
+            to="/show/show_labs" 
+            color="inherit" 
+            sx={{ fontSize: '1rem', textTransform: 'capitalize', py: 1, px: 2 }}
+          >
             {t('navbar.labs')}
           </Button>
-          <Button component={Link} to="/show/show_location" color="inherit" sx={{ mx: 2 }}>
+          <Button 
+            component={Link} 
+            to="/show/show_location" 
+            color="inherit" 
+            sx={{ fontSize: '1rem', textTransform: 'capitalize', py: 1, px: 2 }}
+          >
             {t('navbar.location')}
           </Button>
-          <Button component={Link} to="/show/show_tests" color="inherit" sx={{ mx: 2 }}>
+          <Button 
+            component={Link} 
+            to="/show/show_tests" 
+            color="inherit" 
+            sx={{ fontSize: '1rem', textTransform: 'capitalize', py: 1, px: 2 }}
+          >
             {t('navbar.tests')}
           </Button>
-          <Button component={Link} to="/show/show_testcategory" color="inherit" sx={{ mx: 2 }}>
+          <Button 
+            component={Link} 
+            to="/show/show_testcategory" 
+            color="inherit" 
+            sx={{ fontSize: '1rem', textTransform: 'capitalize', py: 1, px: 2 }}
+          >
             {t('navbar.testcategory')}
           </Button>
-          <Button component={Link} to="/show/show_paymentMethod" color="inherit" sx={{ mx: 2 }}>
+          <Button 
+            component={Link} 
+            to="/show/show_paymentMethod" 
+            color="inherit" 
+            sx={{ fontSize: '1rem', textTransform: 'capitalize', py: 1, px: 2 }}
+          >
             {t('navbar.paymentMethod')}
           </Button>
-          <Button component={Link} to="/show/show_nurses" color="inherit" sx={{ mx: 2 }}>
+          <Button 
+            component={Link} 
+            to="/show/show_nurses" 
+            color="inherit" 
+            sx={{ fontSize: '1rem', textTransform: 'capitalize', py: 1, px: 2 }}
+          >
             {t('navbar.nurses')}
           </Button>
-        </Container>
+        </Box>
       </Toolbar>
     </AppBar>
   );
